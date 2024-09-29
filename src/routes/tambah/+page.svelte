7151b60a596f53e66c8839a8a482486170455680
hotspoon <fariez18@outlook.com>
2024-09-29 14:18:28 +0700

    Remove unused imports in +page.svelte to streamline code

--- src/routes/tambah/+page.svelte
+++ src/routes/tambah/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-  import * as Table from '$lib/components/ui/table';
-  import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
-  import * as Dialog from '$lib/components/ui/dialog';
-  import Button from '$lib/components/ui/button/button.svelte';
-  import Label from '$lib/components/ui/label/label.svelte';
-  import Input from '$lib/components/ui/input/input.svelte';
-  import { SquareUserRound, Plus, EllipsisVertical, Check, X } from 'lucide-svelte';
+  import { SquareUserRound } from 'lucide-svelte';
   import AddPatientForm from '$lib/components/AddPatientForm.svelte';
 </script>
 
